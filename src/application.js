10c21c8a365d2e296b9f6463216a8ef67d8fbcb2
Natam Oliveira <natamvo@gmail.com>
2020-01-18 13:51:12 -0300

    add the folder structure

--- src/application.js
+++ src/application.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from 'resources/img/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
